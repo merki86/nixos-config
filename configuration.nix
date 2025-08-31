5d4162d09fb4d1a04ec63c78e9fe8915530fa438
merki86 <nassen.d230@gmail.com>
2025-09-01 00:37:35 +0500

    japanese ime support 5

--- configuration.nix
+++ configuration.nix
@@ -29,7 +29,13 @@
 
   # IME for japanese
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-gtk
+      fcitx5-configtool
+    ];
   };
 
   # Enable CUPS to print documents.
